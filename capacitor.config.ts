9699b4735dc3a32dcf795f26c272fec901971ca8
Jeremy Budgeon <jeremybudgeon@gmail.com>
2026-02-01 12:37:55 -0500

    capacitor + remote files + ios changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,21 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tripod.app',
   appName: 'Tripod',
-  webDir: 'dist/tripod'
+  webDir: 'dist/tripod',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      showSpinner: false,
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#111111',
+      launchFadeOutDuration: 300,
+    },
+  },
+  ios: {
+    contentInset: 'automatic',
+  },
 };
 
 export default config;
